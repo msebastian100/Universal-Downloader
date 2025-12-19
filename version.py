2eb25be6dc908471a5b2ebc6673855a0dc155882
Sebastian Mallach <basti-unterwegs@mallach.de>
2025-12-19 20:07:19 +0100

    Bump version to 2.0.5

--- version.py
+++ version.py
@@ -4,8 +4,8 @@
 Versionsinformationen für Universal Downloader
 """
 
-__version__ = "2.0.4"
-__version_info__ = (2, 0, 4)
+__version__ = "2.0.5"
+__version_info__ = (2, 0, 5)
 __build_date__ = "2025-01-19"
 
 # GitHub Repository URL (für Links und Updates)
